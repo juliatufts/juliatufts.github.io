28b017532ceda87b5d7e297cd437d3f91ad1521f
Julia Tufts <juliatufts@gmail.com>
2020-02-09 15:53:51 -0500

    handle touch events for mobile

--- arrow/main.js
+++ arrow/main.js
@@ -363,6 +363,30 @@ function collision(pos, rect) {
   return !clear;
 }
 
+function touchHandler(event)
+{
+    var touches = event.changedTouches,
+        first = touches[0],
+        type = "";
+    switch(event.type)
+    {
+        case "touchstart": type = "mousedown"; break;
+        case "touchmove":  type = "mousemove"; break;
+        case "touchend":   type = "mouseup";   break;
+        default:           return;
+    }
+    var simulatedEvent = document.createEvent("MouseEvent");
+    simulatedEvent.initMouseEvent(type, true, true, window, 1,
+                                  first.screenX, first.screenY,
+                                  first.clientX, first.clientY, false,
+                                  false, false, false, 0/*left*/, null);
+
+    first.target.dispatchEvent(simulatedEvent);
+    if (event.type === "touchstart") {
+      event.preventDefault();
+    }
+}
+
 function init() {
   time = performance.now() / 1000;
 
@@ -374,6 +398,11 @@ function init() {
   arrow.update();
   arrow.render();
   document.getElementById('arrowPoly').setAttributeNS(null, 'fill', 'black');
+
+  document.addEventListener("touchstart", touchHandler, true);
+  document.addEventListener("touchmove", touchHandler, true);
+  document.addEventListener("touchend", touchHandler, true);
+  document.addEventListener("touchcancel", touchHandler, true);
 
   document.addEventListener("mousemove", function(e) {
     mouse.onMove(e);
